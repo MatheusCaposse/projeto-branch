06dac0965f4a9bc89a19c2cd198ecd5730285e01
matheus <matheus.caposse02@gmail.com>
2024-10-03 11:24:35 -0300

    criando função em JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // Estrutura de repetição
 
-for (let i=0; i < 10; i ++)
-    console.log(i)
+//for (let i=0; i < 10; i ++)
+    //console.log(i)
 
-let f = 0;
-while (f<10){
-    console.log(f)
-    f++
+
+//let f = 0;
+//while (f<10){
+    //console.log(f)
+    //f++
+//}
+
+//do while
+let w = 0;
+
+do {
+    console.log(w);
+    w++
 }
+while(w<5)
+
+//funções 
+
+function ola(nome){
+    console.log(`seja bem vindo a ${nome}` ); //strings literal ou template $() chama a varivavel
+    alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+ola("fiap")
